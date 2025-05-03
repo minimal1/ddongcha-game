1a400699f2ce8d6e9f9514ade976291d2c80f5bb
미니멀 <jcm940308@gmail.com>
2025-05-03 13:49:22 +0900

    Update _app.tsx with Supabase Provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import type { AppProps } from 'next/app'
 import Layout from '@/components/Layout'
+import { SupabaseProvider } from '@/shared/context/SupabaseProvider'
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <SupabaseProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </SupabaseProvider>
   )
 }
