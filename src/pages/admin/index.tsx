9314056d6ba0a11f995d077409bffa5de8ad284c
미니멀 <jcm940308@gmail.com>
2025-05-05 03:05:56 +0900

    Update admin index page with AdminLayout

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,20 +1,23 @@
 import React from 'react';
 import Head from 'next/head';
-import AdminAuthGuard from '@/features/admin/auth/ui/AdminAuthGuard';
-
+import AdminLayout from '@/features/admin/layout/AdminLayout';
 
 /**
- * 관리자 대시보드 페이지
+ * 관리자 대시보드/목록 페이지
  */
 const AdminPage: React.FC = () => {
   return (
-    <AdminAuthGuard>
+    <AdminLayout title="퀴즈 관리 대시보드">
       <Head>
-        <title>게임 관리자 페이지</title>
-        <meta name="description" content="레크리에이션 게임 관리자 페이지" />
+        <title>퀴즈 관리자 페이지</title>
+        <meta name="description" content="똥차지기 퀴즈 관리자 페이지" />
       </Head>
-
-    </AdminAuthGuard>
+      
+      {/* 이 부분은 추후 QuizList 컴포넌트로 대체될 예정 */}
+      <div>
+        <p>퀴즈 목록이 이곳에 표시됩니다.</p>
+      </div>
+    </AdminLayout>
   );
 };
 
