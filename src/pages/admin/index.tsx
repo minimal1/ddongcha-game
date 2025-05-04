63ce622d5572b83da91b46610a96594d15ec311c
미니멀 <jcm940308@gmail.com>
2025-05-05 03:13:59 +0900

    Update admin dashboard to use QuizList component

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Head from 'next/head';
 import AdminLayout from '@/features/admin/layout/AdminLayout';
+import QuizList from '@/features/admin/quiz/ui/QuizList';
 
 /**
  * 관리자 대시보드/목록 페이지
@@ -13,10 +14,7 @@ const AdminPage: React.FC = () => {
         <meta name="description" content="똥차지기 퀴즈 관리자 페이지" />
       </Head>
       
-      {/* 이 부분은 추후 QuizList 컴포넌트로 대체될 예정 */}
-      <div>
-        <p>퀴즈 목록이 이곳에 표시됩니다.</p>
-      </div>
+      <QuizList />
     </AdminLayout>
   );
 };
